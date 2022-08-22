db09873f2b68296c24b745ee4c80699bc0a05a8e
CoderY <77945277+yang92649898@users.noreply.github.com>
2022-08-22 20:24:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def health():
 
 
 def lucky():
-  url = "http://api.tianapi.com/star/index?key=" + API_KEY_LUCKY +"&astro="+astro
+  url = "http://api.tianapi.com/star/index?key=" + api_key_lucky +"&astro="+astro
   res = requests.get(url).json()
   data = "\n爱情指数："+str(res["newslist"][1]["content"])+"   工作指数："+str(res["newslist"][2]["content"])+"\n今日概述："+str(res["newslist"][8]["content"])
   return data
