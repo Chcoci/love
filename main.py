dee793e183bb98405440324666835b21560d5593
CoderY <77945277+yang92649898@users.noreply.github.com>
2022-08-22 19:53:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+from requests import get, post
+import sys
+import os
+import http.client, urllib
+import json
+from zhdate import ZhDate
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -16,7 +22,113 @@ app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
+api_key_lucky = os.environ["API_KEY_LUCKY"]
+astro = os.environ["ASTRO"]
+#词霸每日一句
+def get_ciba():
+    if (Whether_Eng!="否"):
+        url = "http://open.iciba.com/dsapi/"
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        }
+        r = get(url, headers=headers)
+        note_en = r.json()["content"]
+        note_ch = r.json()["note"]
+        return note_ch, note_en
+    else:
+        return "",""
 
+
+#彩虹屁
+def caihongpi():
+    if (caihongpi_API!="否"):
+        conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
+        params = urllib.parse.urlencode({'key':caihongpi_API})
+        headers = {'Content-type':'application/x-www-form-urlencoded'}
+        conn.request('POST','/caihongpi/index',params,headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data)
+        data = data["newslist"][0]["content"]
+        if("XXX" in data):
+            data.replace("XXX","蒋蒋")
+        return data
+    else:
+        return ""
+
+#健康小提示API
+def health():
+    if (health_API!="否"):
+        conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
+        params = urllib.parse.urlencode({'key':health_API})
+        headers = {'Content-type':'application/x-www-form-urlencoded'}
+        conn.request('POST','/healthtip/index',params,headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data)
+        data = data["newslist"][0]["content"]
+        return data
+    else:
+        return ""
+
+#星座运势
+def lucky():
+    if (API_KEY_LUCKY!="否"):
+        conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
+        params = urllib.parse.urlencode({'key':API_KEY_LUCKY,'astro':astro})
+        headers = {'Content-type':'application/x-www-form-urlencoded'}
+        conn.request('POST','/star/index',params,headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data)
+        data = "\n爱情指数："+str(data["newslist"][1]["content"])+"   工作指数："+str(data["newslist"][2]["content"])+"\n今日概述："+str(data["newslist"][8]["content"])
+        return data
+    else:
+        return ""
+
+#励志名言
+def lizhi():
+    if (lizhi_API!="否"):
+        conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
+        params = urllib.parse.urlencode({'key':lizhi_API})
+        headers = {'Content-type':'application/x-www-form-urlencoded'}
+        conn.request('POST','/lzmy/index',params,headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data)
+        return data["newslist"][0]["saying"]
+    else:
+        return ""
+        
+
+#下雨概率和建议
+def tip():
+    if (tianqi_API!="否"):
+        conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
+        params = urllib.parse.urlencode({'key':tianqi_API,'city':city})
+        headers = {'Content-type':'application/x-www-form-urlencoded'}
+        conn.request('POST','/tianqi/index',params,headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data)
+        pop = data["newslist"][0]["pop"]
+        tips = data["newslist"][0]["tips"]
+        return pop,tips
+    else:
+        return "",""
+
+#推送信息
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips, note_en, note_ch, health_tip, lucky_):
+    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+    year = localtime().tm_year
+    month = localtime().tm_mon
+    day = localtime().tm_mday
+    today = datetime.date(datetime(year=year, month=month, day=day))
+    week = week_list[today.isoweekday() % 7]
+    # 获取在一起的日子的日期格式
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
@@ -58,6 +170,7 @@ data = {"city":{"value":city},
         "high":{"value":high,"color":get_random_color()},
         "love_days":{"value":get_count(),"color":get_random_color()},
         "birthday_left":{"value":get_birthday(),"color":get_random_color()},
-        "words":{"value":get_words(), "color":get_random_color()}}
+        "words":{"value":get_words(), "color":get_random_color()},
+        "lucky":{"value":lucky(),"color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
