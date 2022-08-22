16ba9de7d7888798a1f50f40b1b8d3db52af49ec
CoderY <77945277+yang92649898@users.noreply.github.com>
2022-08-22 17:07:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today+1).days
+  return (next - today).days + 1
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
