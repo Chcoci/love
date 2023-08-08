3e607cecb386586d281e069a2ffd7cc55693a035
CoderY <77945277+Chcoci@users.noreply.github.com>
2023-08-08 10:01:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,7 @@ data2 = {"city":{"value":city2},
 
 #res = wm.send_template(user_id, template_id, data)  #发送
 res2 = wm.send_template(user_id2, template_id2, data2)
+res2 = wm.send_template(user_id2, template_id, data)
 #print(res)
 print(data2)
 print(res2)
