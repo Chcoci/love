8325ded213007ea513b7d06f5e1bcc93305550c2
CoderY <77945277+Chcoci@users.noreply.github.com>
2023-08-08 11:10:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def lucky(): # 女方星座
 def lucky2(): # 男方星座
    url = "http://api.tianapi.com/star/index?key=" + api_key_lucky +"&astro="+astro2
    res = requests.get(url).json()
-   data = "男 test 2 星座"
+   data = "\n爱情指数："+str(res["newslist"][1]["content"])+"\t工作指数："+str(res["newslist"][2]["content"])+"\n财运指数："+str(res["newslist"][3]["content"])+"\t健康指数："+str(res["newslist"][4]["content"])+"\n今日概述："+str(res["newslist"][8]["content"])
    return data
 # "\n爱情指数："+str(res["newslist"][1]["content"])+"\t工作指数："+str(res["newslist"][2]["content"])+"\n财运指数："+str(res["newslist"][3]["content"])+"\t健康指数："+str(res["newslist"][4]["content"])+"\n今日概述："+str(res["newslist"][8]["content"])
 
